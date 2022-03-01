0c2fb6b3b603e0c4ef02550a5b4d9d733bbfb58c
Fleetime <novian.andika@students.amikom.ac.id>
2022-03-01 13:55:39 +0700

    feat: simple create read update delete

--- app/Controllers/Latihan.php
+++ app/Controllers/Latihan.php
@@ -34,4 +34,55 @@ class Latihan extends BaseController
         $hasil = $a / $b;
         echo 'Hasil pembagian ' . $a . ' / ' . $b . ' = ' . $hasil;
     }
+
+    public function ambil()
+    {
+        $db = \Config\Database::connect();
+
+        $builder = $db->table('kota');
+        $hasil = $builder->get();
+
+        foreach ($hasil->getResult() as $row) {
+            echo $row->kota_nama . '<br>';
+        }
+    }
+
+    public function tambah()
+    {
+        $db = \Config\Database::connect();
+
+        $builder = $db->table('kota');
+
+        $builder->insert([
+            'kota_nama' => 'Surakarta', // Masukkan data kota
+        ]);
+
+        echo 'Data berhasil ditambahkan';
+    }
+
+    public function ubah()
+    {
+        $db = \Config\Database::connect();
+
+        $builder = $db->table('kota');
+
+        $builder->where('kota_id', 2); // Masukkan data kota
+        $builder->update([
+            'kota_nama' => 'Bogor', // Masukkan data kota
+        ]);
+
+        echo 'Data berhasil diubah';
+    }
+
+    public function hapus()
+    {
+        $db = \Config\Database::connect();
+
+        $builder = $db->table('kota');
+
+        $builder->where('kota_id', 3); // Masukkan data kota
+        $builder->delete();
+
+        echo 'Data berhasil dihapus';
+    }
 }

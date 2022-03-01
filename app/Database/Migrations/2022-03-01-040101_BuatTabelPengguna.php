fbc95b065d8dedbe392c1567194b7d44aab90dfa
Fleetime <novian.andika@students.amikom.ac.id>
2022-03-01 11:31:18 +0700

    feat: add migration

--- app/Database/Migrations/2022-03-01-040101_BuatTabelPengguna.php
+++ app/Database/Migrations/2022-03-01-040101_BuatTabelPengguna.php
@@ -8,11 +8,21 @@ class BuatTabelPengguna extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'pengguna_id' => ['type' => 'INT', 'unsigned' => true, 'auto_increment' => true],
+            'pengguna_email' => ['type' => 'VARCHAR', 'constraint' => '100', 'unique' => true],
+            'pengguna_sandi' => ['type' => 'VARCHAR', 'constraint' => '100'],
+            'kota_id' => ['type' => 'INT', 'unsigned' => true],
+        ]);
+
+        $this->forge->addKey('pengguna_id', true);
+        // Kota_id di tabel pengguna terhubung dengan tabel kota pada kolom kota_id
+        $this->forge->addforeignKey('kota_id', 'kota', 'kota_id');
+        $this->forge->createTable('pengguna');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('pengguna');
     }
 }
